41e3e23753831345111f20edfe284d0b8498cd16
nixswinner <nixondeveloper@gmail.com>
2020-07-29 09:55:56 +0300

    initial commit

---
+++ app/src/main/java/com/mitclass/scoreboard/MainActivity.java
@@ -0,0 +1,26 @@
+package com.mitclass.scoreboard;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /*
+    * First We have an Activity - Someone will create teams - 2teams Names - teamA,teamB
+    * - teamAScore = 0
+    * - teamBScore = 0
+    * -ScoreBoard - Activity - we would recording
+    * -Summary - We have all Teams Scores ofcourse the winner
+    * Alert
+    *
+    * -
+    *
+    * */
+}
---
+++ app/src/main/java/com/mitclass/scoreboard/ScoresActivity.java
@@ -0,0 +1,14 @@
+package com.mitclass.scoreboard;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ScoresActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scores);
+    }
+}

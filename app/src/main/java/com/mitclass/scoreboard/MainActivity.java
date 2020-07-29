b315eddf3793b0dafce78c96336c1ac85f77f968
nixswinner <nixondeveloper@gmail.com>
2020-07-29 11:48:07 +0300

    add teams track score activity

--- app/src/main/java/com/mitclass/scoreboard/MainActivity.java
+++ app/src/main/java/com/mitclass/scoreboard/MainActivity.java
@@ -1,15 +1,72 @@
 package com.mitclass.scoreboard;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnSave;
+    private EditText edtNameTeamA,edtNameTeamB;
+    SharedPreferences preferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //view binding
+        edtNameTeamA = findViewById(R.id.edt_teamA);
+        edtNameTeamB = findViewById(R.id.edt_teamB);
+
+        preferences = getApplicationContext().getSharedPreferences("score_pref",MODE_PRIVATE);
+
+        String teamA = preferences.getString("teamA",null);
+        if (teamA!=null){
+            startActivity(new Intent(MainActivity.this,ScoresActivity.class));
+            finish();
+        }
+
+        btnSave = findViewById(R.id.btnSave);
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //check if name A and B
+                //
+                if (edtNameTeamA.getText().toString().isEmpty()){
+                    edtNameTeamA.setError("Kindly Provide name of Team A");
+                }else {
+                    if (edtNameTeamB.getText().toString().isEmpty()){
+                        edtNameTeamB.setError("Kindly Provide name of Team B");
+                    }else{
+                        saveTeams(edtNameTeamA.getText().toString(),edtNameTeamB.getText().toString());
+                    }
+                }
+            }
+        });
+    }
+
+    private void saveTeams(String teamA,String teamB){
+        //save these teams A, initialiaze
+        //instantiate the sharedpreference
+      //   preferences = getApplicationContext().getSharedPreferences("score_pref",MODE_PRIVATE);
+        //instance
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("teamA",teamA);
+        editor.putString("teamB",teamB);
+        //initiliazes scores values
+        editor.putString("teamAScore","0");
+        editor.putString("teamBScore","0");
+        //commit
+        editor.commit();
+
+        //finish this activity
+        finish();
+        startActivity(new Intent(MainActivity.this,ScoresActivity.class));
+
     }
 
     /*
--- app/src/main/java/com/mitclass/scoreboard/ScoresActivity.java
+++ app/src/main/java/com/mitclass/scoreboard/ScoresActivity.java
@@ -1,14 +1,84 @@
 package com.mitclass.scoreboard;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ScoresActivity extends AppCompatActivity {
 
+    TextView txtTeamAName,txtTeamBName,txtScoreA,txtScoreB;
+    ImageButton imgAddScoreA,imgAddScoreB;
+    Button btnGameOver;
+    SharedPreferences preferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scores);
+
+        preferences = getApplicationContext().getSharedPreferences("score_pref",MODE_PRIVATE);
+
+        txtTeamAName = findViewById(R.id.txtTeamAName);
+        txtTeamBName = findViewById(R.id.txtTeamBName);
+        txtScoreA = findViewById(R.id.txtTeamAScore);
+        txtScoreB = findViewById(R.id.txtTeamBScore);
+
+        imgAddScoreA = findViewById(R.id.imgBtnAddScoreA);
+        imgAddScoreB = findViewById(R.id.imgBtnAddScoreB);
+
+        imgAddScoreA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //we shall increament score value by one the update on shared prefernce
+                //get value of score
+                //increament by 1
+                //save the result
+                //String key ="teamAScore";
+                saveScore("teamAScore");
+
+            }
+        });
+
+        imgAddScoreB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String key ="teamBScore";
+                saveScore(key);
+            }
+        });
+
+        //get team A
+        txtTeamAName.setText(preferences.getString("teamA",null));
+        //get team B
+        txtTeamBName.setText(preferences.getString("teamB",null));
+
+        //get scores
+        txtScoreA.setText(preferences.getString("teamAScore","0"));
+        txtScoreB.setText(preferences.getString("teamBScore","0"));
+
+        //get the teams scores and the names prefences
+    }
+
+    private void saveScore(String key){
+        String currentScore  = preferences.getString(key,"0");
+        int result = Integer.parseInt(currentScore)+1;
+        //save the result
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString(key,""+result);
+        editor.commit();
+
+        //
+        Toast.makeText(this,"Score added to "+key,Toast.LENGTH_SHORT).show();
+        //update the user interface
+        if (key.contains("teamAScore")){
+            txtScoreA.setText(""+result);
+        }else {
+            txtScoreB.setText(String.valueOf(result));
+        }
     }
 }
